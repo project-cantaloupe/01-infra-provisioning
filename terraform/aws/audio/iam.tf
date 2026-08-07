ef0ba961c6ede45734d9f9d11c2a6bfb2e355ce8
0hJieun <wantnest24@gmail.com>
2026-08-07 15:12:27 +0900

    feat(finops): allow worker to read CloudWatch metrics

--- terraform/aws/audio/iam.tf
+++ terraform/aws/audio/iam.tf
@@ -243,3 +243,33 @@ resource "aws_iam_role_policy" "audio_node" {
   role   = data.terraform_remote_state.compute[0].outputs.worker_role_name
   policy = data.aws_iam_policy_document.audio_node[0].json
 }
+
+# CloudWatch Exporter는 AWS service Worker에서 실행하고 EC2 Instance Profile의
+# 단기 자격증명을 사용한다. S3 객체나 로그를 읽지 않고 CloudWatch metric과
+# exporter 검증에 필요한 EC2 metadata만 조회한다.
+data "aws_iam_policy_document" "finops_cloudwatch_read" {
+  count = var.enable_node_role_policy ? 1 : 0
+
+  statement {
+    sid    = "ReadCloudWatchMetrics"
+    effect = "Allow"
+    actions = [
+      "cloudwatch:GetMetricData",
+      "cloudwatch:GetMetricStatistics",
+      "cloudwatch:ListMetrics",
+      "ec2:DescribeInstances",
+      "ec2:DescribeRegions",
+      "ec2:DescribeTags",
+      "tag:GetResources",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "finops_cloudwatch_read" {
+  count = var.enable_node_role_policy ? 1 : 0
+
+  name   = "cntlp-finops-cloudwatch-read"
+  role   = data.terraform_remote_state.compute[0].outputs.worker_role_name
+  policy = data.aws_iam_policy_document.finops_cloudwatch_read[0].json
+}
