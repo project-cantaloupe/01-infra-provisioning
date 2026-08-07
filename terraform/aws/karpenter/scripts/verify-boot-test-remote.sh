359c0301c1be466ab4712e1f0117139e4bb599ed
Evan <86917717+kyunghoon02@users.noreply.github.com>
2026-08-07 22:14:17 +0900

    test(karpenter): Bootstrap AMI 부팅 검증 확장

--- terraform/aws/karpenter/scripts/verify-boot-test-remote.sh
+++ terraform/aws/karpenter/scripts/verify-boot-test-remote.sh
@@ -7,6 +7,7 @@ test "$(hostname)" = "cntlp-aws-wk-99"
 grep -Eq '^[0-9a-f]{32}$' /etc/machine-id
 test -f /etc/ssh/ssh_host_ed25519_key.pub
 test ! -e /etc/kubernetes/kubelet.conf
+test ! -e /var/lib/cntlp/bootstrap-complete
 test "$(find /etc/cni/net.d -type f 2>/dev/null | wc -l | tr -d ' ')" = "0"
 test "$(swapon --show --noheadings | wc -l | tr -d ' ')" = "0"
 
@@ -26,6 +27,9 @@ test "${tailscale_backend}" != "Running"
 
 test "$(kubeadm version -o short)" = "v1.36.3"
 test "$(kubelet --version | awk '{print $2}')" = "v1.36.3"
+test -x /usr/local/sbin/cntlp-worker-bootstrap
+bash -n /usr/local/sbin/cntlp-worker-bootstrap
+aws --version 2>&1 | grep -Fq 'aws-cli/2.36.18'
 
 printf 'hostname=%s\n' "$(hostname)"
 printf 'machine_id=regenerated\n'
@@ -35,6 +39,8 @@ printf 'swap=disabled\n'
 printf 'containerd=%s\n' "$(containerd --version)"
 printf 'kubeadm=%s\n' "$(kubeadm version -o short)"
 printf 'kubelet=%s\n' "$(kubelet --version | awk '{print $2}')"
+printf 'aws_cli=%s\n' "$(aws --version 2>&1 | awk '{print $1}')"
+printf 'bootstrap_runner=ready\n'
 printf 'kubelet_active=%s\n' "$(systemctl is-active kubelet || true)"
 printf 'tailscale=%s\n' "$(tailscale version | head -1)"
 printf 'tailscale_backend=%s\n' "${tailscale_backend}"
