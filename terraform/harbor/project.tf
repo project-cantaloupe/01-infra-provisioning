0581e4ca27d5cfd9866e75276fc10ee93a606eba
pneuma <40439319+aggj153@users.noreply.github.com>
2026-08-12 17:51:00 +0900

    feat: add harbor webhook for trivy scan results

--- terraform/harbor/project.tf
+++ terraform/harbor/project.tf
@@ -5,3 +5,15 @@ resource "harbor_project" "library" {
   deployment_security    = "high"
   vulnerability_scanner  = "Trivy"
 }
+
+resource "harbor_project_webhook" "trivy_fluentbit" {
+  name        = "trivy-to-fluentbit"
+  address     = "http://fluent-bit-webhook.logging.svc.cluster.local:9880/harbor.webhook"
+  project_id  = harbor_project.library.id
+  events_types = [
+    "SCANNING_COMPLETED",
+    "SCANNING_FAILED"
+  ]
+  notify_type = "http"
+  enabled     = true
+}
